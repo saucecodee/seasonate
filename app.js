ba5663dd4a08ca49ee9b3036fece3f41ea0507d8
saucecodee <ashawinta@gmail.com>
2021-01-02 11:00:14 +0100

    chore: add comments

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const fs = require("fs")
 
+// Current working directory
 const cwd = process.cwd()
+// Get all files in the cwd
 const files = fs.readdirSync(cwd)
 
-
+// Loop through all the files
 files.forEach(file => {
-     const regEx = /^(.*)(S([\d]{1,})E([\d]{1,})).*(\.[\w]{2,})$/
-     
-     // Make sure the file name is valid
-     if (regEx.test(file)) {
-          const groups = regEx.exec(file)
+     // Regex for valid filenames
+     const movieNameRegEx = /^(.*)(S([\d]{1,})E([\d]{1,})).*(\.[\w]{2,})$/
+
+     // check if the file name matches the movie regex
+     if (movieNameRegEx.test(file)) {
+          const groups = movieNameRegEx.exec(file)
 
           const movieName = groups[1].split(/[._-]/).join(" ").trim()
           const season = groups[3]
           const episode = groups[4]
-          const fileExt = groups[5]
-
+          const fileExtention = groups[5]
 
           const oldMoviePath = `${cwd}/${file}`
           const newMoviePath = `${cwd}/${movieName}/Season-${season}`
-          const newMovieName = `/${movieName}-S${season}E${episode}${fileExt}`
+          const newMovieName = `/${movieName}-S${season}E${episode}${fileExtention}`
 
-          // Create new directory if it doesn't exist
-          if (!fs.existsSync(newMoviePath)) {
-               fs.mkdirSync(newMoviePath, { recursive: true });
-          }
+          // Create new file directory if it doesn't exist
+          if (!fs.existsSync(newMoviePath)) fs.mkdirSync(newMoviePath, { recursive: true });
 
-          // Copy movie to the new dir
+          // Copy movie to the the new directory
           fs.copyFile(oldMoviePath, newMoviePath + newMovieName, (err) => {
                if (err) throw err;
                console.log(`[Copied] ${movieName} -  Season ${season} Episode ${episode}`);
